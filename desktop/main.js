76e44b6f206f690a00899f47f2accf3ad78556f0
Nathan Tchinda <nathan.tchinda@transfertgratis.com>
2026-06-12 10:33:10 +0100

    update

--- desktop/main.js
+++ desktop/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, dialog, utilityProcess } = require('electron');
+const { app, BrowserWindow, dialog, utilityProcess, ipcMain } = require('electron');
 const { spawn } = require('child_process');
 const path = require('path');
 const fs = require('fs');
