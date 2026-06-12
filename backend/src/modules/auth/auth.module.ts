64048fe7f3a3685c3b563b1929dd51f8628b3c31
Nathan Tchinda <nathan.tchinda@transfertgratis.com>
2026-06-12 17:07:04 +0100

    add jwt expiration

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { MailModule } from '../mail/mail.module';
             inject: [ConfigService],
             useFactory: (config: ConfigService) => ({
                 secret: config.get('JWT_SECRET'),
-                signOptions: { expiresIn: config.get('JWT_EXPIRATION') || '15m' },
+                signOptions: { expiresIn: config.get('JWT_EXPIRATION') || '8h' },
             }),
         }),
         MailModule,
